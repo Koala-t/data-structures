9973f16a1bb642ddf1d1c55d2f5205b4564b09c5
Koala-t <johnsonc.88@gmail.com>
2016-03-02 14:37:54 -0800

    tracks the number of elements added to the object

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,16 +3,18 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var item = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    item++;
   };
 
   someInstance.pop = function() {
   };
 
   someInstance.size = function() {
-    return 0;
+    return item;
   };
 
   return someInstance;
