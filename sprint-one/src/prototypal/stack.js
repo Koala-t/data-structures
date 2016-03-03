8f6d0b3e77c010e241714cb836a55b67a9294b01
Koala-t <johnsonc.88@gmail.com>
2016-03-03 10:57:16 -0800

    tracks the size of the stack

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,26 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(stackMethods);
+  someInstance.count = 0;
+
+  return someInstance;
 };
+//QUESTION
+//why can we not use Object.create(Stack.prototype)
+//and Stack.prototype.size = function(){return 0;}
+//for the first test?
 
 var stackMethods = {};
 
+stackMethods.size = function(){
+  return this.count;
+};
 
+stackMethods.push = function(){
+  this.count++;
+};
+
+stackMethods.pop = function(){
+  if(this.count > 0){
+    this.count--;
+  }
+};
