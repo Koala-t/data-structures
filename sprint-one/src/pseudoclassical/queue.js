d327efea83422dd111a947e6c5f0ca4636f8eee1
Koala-t <johnsonc.88@gmail.com>
2016-03-03 12:26:16 -0800

    add and remove elements to the queue

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,28 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.count = 0;
+  this.storage = {};
 };
+
+Queue.prototype.size = function() {
+  return this.count;
+};
+
+Queue.prototype.enqueue = function(value) {
+  this.storage[this.count] = value;
+  this.count++;
+}
+
+Queue.prototype.dequeue = function (){
+  if(this.count>0){
+    this.count--;
+  }
+  var temp = this.storage[0];
+  //iterate through the queue starting on the second element
+  for (i=1; i<=this.count; i++){
+    this.storage[i-1] = this.storage[i];
+  }
+  delete this.storage[this.count];
+  return temp;
+}
 
 
