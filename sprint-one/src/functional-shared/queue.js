aa0f9e496e6bb8c0e7884c7dfc77c31ad3dabb82
Koala-t <johnsonc.88@gmail.com>
2016-03-02 21:40:47 -0800

    Ability to add items to the queue

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,38 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  someInstance.count = 0;
+  someInstance.storage = {};
+  extend(someInstance, Queue.queueMethods);
+  return someInstance;
 };
 
-var queueMethods = {};
+Queue.queueMethods = {
+  size: function(){
+    return this.count;
+  },
+  enqueue: function(value){
+    this.storage[this.count] = value;
+    this.count++;
+  },
+  dequeue: function(){
+    if(this.count !== 0){this.count--;}
+    
+    var temp = this.storage[0];
+    
+    for(var i=1; i<= this.count; i++){
+      this.storage[i] = this.storage[i-1];
+    }
+    
+    delete this.storage[this.count];
+
+    return temp;
+  }
+};
 
 
+
+var extend = function(to, from){
+  for(var key in from){
+    to[key] = from[key];
+  }
+};
