768917e6719e901f6383e0df7283afe31eb4e21a
Koala-t <johnsonc.88@gmail.com>
2016-03-03 22:14:24 -0800

    Completed linkedList to pass tests

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,14 +4,50 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    //if this is the first node to be added to the list
+    if (list.tail === null && list.head === null) {
+      //invoke Node and assing the resulting node-object to the tail and head
+      list.tail = Node(value);
+      list.head = list.tail;
+      //otherwise
+    } else {
+    // generate new node and set it to list.tail.next temporarily
+    list.tail.next = Node(value);
+    // set list.tail to the newly generated node
+    list.tail = list.tail.next;
+    }
+
   };
 
   list.removeHead = function() {
+    
+    //test if empty
+    if (list.head !== null) {
+    // store head to be removed 
+    var formerHead = list.head;
+    list.head = list.head.next;
+    return formerHead.value;
+    }
   };
 
   list.contains = function(target) {
-  };
+    var found = false;
 
+    var search = function(suspect) {
+
+      if (suspect !== null) { 
+        if (target === suspect.value) {
+          found = true;
+          return found;
+        } else {
+          return search(suspect.next);
+        }
+      }
+      return found;
+    };
+    return search(list.head);
+
+  };
   return list;
 };
 
@@ -21,9 +57,17 @@ var Node = function(value) {
   node.value = value;
   node.next = null;
 
+
   return node;
 };
+
+
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+
+
+
+
