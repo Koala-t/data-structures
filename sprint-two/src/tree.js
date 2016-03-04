6f57e4e96ac2714d554625602f5e50a7c698abbf
Koala-t <johnsonc.88@gmail.com>
2016-03-04 11:20:06 -0800

    Added ability to add children to tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,10 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
+  extend(newTree, treeMethods);
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
 
   return newTree;
 };
@@ -11,11 +12,19 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  // create master node of new tree and push to children array
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target) {
 };
 
+// extend function
+var extend = function(to, from){
+  for (var key in from){
+    to[key] = from[key];
+  }
+};
 
 
 /*
