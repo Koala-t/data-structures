f4a81fd9d5f63bcd200203ec931a6984a569d24a
Koala-t <johnsonc.88@gmail.com>
2016-03-04 23:40:27 -0800

    Refactored stack (pseudoclassical) to pass all tests

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,13 +1,14 @@
 var Stack = function() {
   this.count = 0;
-
+  this.storage = {};
 };
 
 Stack.prototype.size = function(){
   return this.count;
 };
 
-Stack.prototype.push = function(){
+Stack.prototype.push = function(value){
+  this.storage[this.count] = value;
   this.count++;
 };
 
@@ -15,4 +16,7 @@ Stack.prototype.pop = function(){
   if(this.count > 0){
     this.count--;
   }
+  var temp = this.storage[this.count];
+  delete this.storage[this.count];
+  return temp;
 };
