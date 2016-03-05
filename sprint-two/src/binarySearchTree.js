bff64975b1aeb2b23826593016dd91ba4aaee997
Koala-t <johnsonc.88@gmail.com>
2016-03-04 23:27:04 -0800

    Outlined solution for BST

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,25 @@
 var BinarySearchTree = function(value) {
+  var someInstance = Object.create(treeMethods);
+  someInstance.left = null;
+  someInstance.right = null;
+  someInstance.master = value;
+  return someInstance;
+
+
+};
+
+var treeMethods = {};
+
+treeMethods.insert = function(value) {
+
+};
+
+treeMethods.contains = function(target) {
+
+};
+
+treeMethods.depthFirstLog = function(cb) {
+
 };
 
 
