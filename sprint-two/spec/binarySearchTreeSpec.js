715bb89a7f156a4c0833f3b26b15cdb16575d13b
Koala-t <johnsonc.88@gmail.com>
2016-03-05 17:39:46 -0800

    removed a console.log

--- sprint-two/spec/binarySearchTreeSpec.js
+++ sprint-two/spec/binarySearchTreeSpec.js
@@ -13,7 +13,6 @@ describe('binarySearchTree', function() {
 
   it('should insert values at the correct location in the tree', function() {
     binarySearchTree.insert(2);
-    console.log(binarySearchTree);
     binarySearchTree.insert(3);
     binarySearchTree.insert(7);
     binarySearchTree.insert(6);
